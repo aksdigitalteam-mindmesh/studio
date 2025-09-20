6595461d86361fb412f6f47863f4f9b3694546ae
AKS gamer <tejveersingh580@gmail.com>
2025-09-20 16:42:07 +0000

    Make animation slow and calming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -100,11 +100,9 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'pulse-shadow': 'pulse-shadow 2.5s infinite',
+        'pulse-shadow': 'pulse-shadow 4s infinite',
       },
     },
   },
   plugins: [require('tailwindcss-animate')],
 } satisfies Config;
-
-    
