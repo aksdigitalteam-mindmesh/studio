f3cbefaec1aacb6b29beee98a6cf8281441cfb21
AKS gamer <tejveersingh580@gmail.com>
2025-11-07 15:39:37 +0000

    set calorie intake in just left in KCAL Left and calorie burnt in just l

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -187,7 +187,7 @@ export default function DashboardPage() {
       return format(currentDate, 'EEEE, dd MMM').toUpperCase();
     })();
 
-    const caloriesLeft = CALORIE_GOAL - eatenCalories + burnedCalories;
+    const caloriesLeft = CALORIE_GOAL - eatenCalories;
     const eatenProgress = (eatenCalories / CALORIE_GOAL) * 100;
     const burnedProgress = (burnedCalories / CALORIE_GOAL) * 100;
 
