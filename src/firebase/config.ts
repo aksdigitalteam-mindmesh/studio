992d30ff7ee335a03eb5d227aaa9e2bebbda2842
AKS gamer <tejveersingh580@gmail.com>
2025-11-07 14:19:36 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,5 +6,6 @@ export const firebaseConfig = {
   projectId: "fitboost-ktlnv",
   storageBucket: "fitboost-ktlnv.appspot.com",
   messagingSenderId: "899041749945",
-  appId: "1:899041749945:web:e60788b5beaaa568206468"
+  appId: "1:899041749945:web:e60788b5beaaa568206468",
+  measurementId: "G-5WR2WBK5P6"
 };
